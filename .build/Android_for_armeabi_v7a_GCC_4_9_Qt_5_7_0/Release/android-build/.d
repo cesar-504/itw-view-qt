a9c8fe23cd3818f12382d968633aaa06bdc09f00
Cesar Guerra <cesargb504@gmail.com>
2016-11-20 18:09:02 -0800

    creacion de elemnto de interfaz grafica

--- .build/Android_for_armeabi_v7a_GCC_4_9_Qt_5_7_0/Release/android-build/.d
+++
@@ -1,6 +0,0 @@
-/home/cesar/Documentos/Proyectos/old/c++/appView/.build/Android_for_armeabi_v7a_GCC_4_9_Qt_5_7_0/Release/android-build/gen/org/kde/necessitas/ministro/IMinistro.java : \
-  /home/cesar/Documentos/Proyectos/old/c++/appView/.build/Android_for_armeabi_v7a_GCC_4_9_Qt_5_7_0/Release/android-build/src/org/kde/necessitas/ministro/IMinistro.aidl \
-  /home/cesar/Documentos/Proyectos/old/c++/appView/.build/Android_for_armeabi_v7a_GCC_4_9_Qt_5_7_0/Release/android-build/src/org/kde/necessitas/ministro/IMinistroCallback.aidl
-
-/home/cesar/Documentos/Proyectos/old/c++/appView/.build/Android_for_armeabi_v7a_GCC_4_9_Qt_5_7_0/Release/android-build/src/org/kde/necessitas/ministro/IMinistro.aidl :
-/home/cesar/Documentos/Proyectos/old/c++/appView/.build/Android_for_armeabi_v7a_GCC_4_9_Qt_5_7_0/Release/android-build/src/org/kde/necessitas/ministro/IMinistroCallback.aidl :
--- .build/Android_for_armeabi_v7a_GCC_4_9_Qt_5_7_0/Release/android-build/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/cesar/Documentos/Proyectos/old/c++/appView/.build/Android_for_armeabi_v7a_GCC_4_9_Qt_5_7_0/Release/android-build/bin/AndroidManifest.xml : \
-/home/cesar/Documentos/Proyectos/old/c++/appView/.build/Android_for_armeabi_v7a_GCC_4_9_Qt_5_7_0/Release/android-build/AndroidManifest.xml \
